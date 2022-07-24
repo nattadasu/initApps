2c59126b947c9aaefc2a9bacd7904a59479a193c
Natsu Tadama <sultan.ridita@gmail.com>
2022-07-25 00:29:42 +0700

    Create PowerShell's profile

---
+++ config/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,16 @@
+#!/usr/bin/env pwsh
+
+neofetch
+
+Import-Module PSReadLine
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompleteion for Arrow keys
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Set-PSReadLineOption -ShowToolTips
+Set-PSReadLineOption -PredictionSource History
